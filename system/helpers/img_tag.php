01ff4edd87d72a86bf14d42a4637103db5fbac81
Karisa Mwambire <nkmwambs@gmail.com>
2015-06-08 15:49:18 +0300

    Improved URLs

--- system/helpers/img_tag.php
+++ system/helpers/img_tag.php
@@ -2,10 +2,10 @@
 function img_tag($path,$properties=""){
     
     if(file_exists(BASE_PATH.DS."application".DS.$GLOBALS['app'].DS."images".DS.$path)){
-        $str = "<img src='".PATH.DS."application".DS.$GLOBALS['app'].DS."images".DS.$path."'";
+        $str = "<img src='".$_SERVER['HTTP_HOST']."/application/".$GLOBALS['app']."/images/".$path."'";
 
     }  else {
-        $str = "<img src='".PATH.DS."system".DS."images".DS.$path."'"; 
+        $str = "<img src='".$_SERVER['HTTP_HOST']."/system/images/".$path."'"; 
     }
     
 
