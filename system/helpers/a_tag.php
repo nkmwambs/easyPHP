54984c7d8cece84aaa31427b0a8e4c2efe7b2215
Karisa Mwambire <nkmwambs@gmail.com>
2015-06-08 19:48:52 +0300

    Defined an empty a_tag url

--- system/helpers/a_tag.php
+++ system/helpers/a_tag.php
@@ -2,7 +2,12 @@
 function a_tag($path,$text,$properties=""){
     //property array example - array("width"=>"80px","heigth"=>"90px","title"=>"Petty","style"=>"margin-top:10px;border:5px solid black;")
     //$str  = "<a href='http://".filter_input(INPUT_SERVER,"HTTP_HOST")."/easyPHP/".$GLOBALS['app']."/".$path."' ";
-    $str  = "<a href='".PATH.DS.$GLOBALS['app'].DS.$path."' ";
+    if($path===""){
+    	$str  = "<a href='#' ";
+    }else{
+    	$str  = "<a href='".PATH.DS.$GLOBALS['app'].DS.$path."' ";
+    }
+    
     
     if(is_array($properties)){
         foreach ($properties as $key => $value) {
