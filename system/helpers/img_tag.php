d70336243a4cd35c0bf00e644824527da8e652f9
Karisa Mwambire <nkmwambs@gmail.com>
2015-06-08 18:39:38 +0300

    Added http:// to the img helper src url

--- system/helpers/img_tag.php
+++ system/helpers/img_tag.php
@@ -3,10 +3,10 @@
 function img_tag($path,$properties=""){
     
     if(file_exists(BASE_PATH.DS."application".DS.$GLOBALS['app'].DS."images".DS.$path)){
-        $str = "<img src='".$_SERVER['HTTP_HOST']."/easyPHP/application/".$GLOBALS['app']."/images/".$path."'";
+        $str = "<img src='http://".$_SERVER['HTTP_HOST']."/easyPHP/application/".$GLOBALS['app']."/images/".$path."'";
 
     }  else {
-        $str = "<img src='".$_SERVER['HTTP_HOST']."/easyPHP/system/images/".$path."'"; 
+        $str = "<img src='http://".$_SERVER['HTTP_HOST']."/easyPHP/system/images/".$path."'"; 
     }
     
 
