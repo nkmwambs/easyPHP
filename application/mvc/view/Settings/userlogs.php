78e1bc08ccebca331d582f58f46eb868b4ec5a0f
Karisa Mwambire <nkmwambs@gmail.com>
2015-08-29 15:26:26 +0300

    Added mailing templates two

---
+++ application/mvc/view/Settings/userlogs.php
@@ -0,0 +1,13 @@
+<?php
+//print_r($data);
+echo "<table id='info_tbl' style='margin-top:25px;'>";
+echo "<tr><th>Cluster</th><th>ICP No</th><th>Last Log Time</th><th># Of Logs</th></tr>";
+foreach($data['logs'] as $value):
+	echo "<tr>";
+		foreach($value as $val):
+			echo "<td>".$val."</td>";
+		endforeach;
+	echo "</tr>";
+endforeach;
+echo "</table>";
+?>
